a8a42dacad217b377f3accdd7d8a28041d72c585
KatriinaTee <katriina.tikkala@student.lut.fi>
2024-07-04 21:38:33 +0300

    Ensimmäinen Android-ohjelma

--- app/src/main/java/com/example/calculator/MainActivity.java
+++ app/src/main/java/com/example/calculator/MainActivity.java
@@ -1,30 +1,79 @@
 package com.example.calculator;
 
 import android.os.Bundle;
-import android.util.Log;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.view.View;
+
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
-    private android.util.Log Log;
+    private EditText editFirstNumber;
+    private EditText editSecondNumber;
+    private TextView textSeeResults;
+    int num1, num2;
+
+    public void sumNumbers(View view) {
+        String s1 = editFirstNumber.getText().toString();
+        // taking input from text box 2
+        String s2 = editSecondNumber.getText().toString();
+        // converting string to int.
+        num1 = Integer.parseInt(s1);
+        // converting string to int.
+        num2 = Integer.parseInt(s2);
+        int sum = num1 + num2;
+        textSeeResults.setText(String.format("%d", sum));
+    }
+
+    public void minusNumbers(View view) {
+        String s1 = editFirstNumber.getText().toString();
+        // taking input from text box 2
+        String s2 = editSecondNumber.getText().toString();
+        // converting string to int.
+        num1 = Integer.parseInt(s1);
+        // converting string to int.
+        num2 = Integer.parseInt(s2);
+        int sum = num1 - num2;
+        textSeeResults.setText(String.format("%d", sum));
+    }
+
+    public void multiplyNumbers(View view) {
+        String s1 = editFirstNumber.getText().toString();
+        // taking input from text box 2
+        String s2 = editSecondNumber.getText().toString();
+        // converting string to int.
+        num1 = Integer.parseInt(s1);
+        // converting string to int.
+        num2 = Integer.parseInt(s2);
+        int sum = num1 * num2;
+        textSeeResults.setText(String.format("%d", sum));
+    }
+
+    public void divideNumbers(View view) {
+        String s1 = editFirstNumber.getText().toString();
+        // taking input from text box 2
+        String s2 = editSecondNumber.getText().toString();
+        // converting string to int.
+        num1 = Integer.parseInt(s1);
+        // converting string to int.
+        num2 = Integer.parseInt(s2);
+        int sum = num1 / num2;
+        textSeeResults.setText(String.format("%d", sum));
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
 
-        Log.d("LUT", "Ohjelma on käynnistynyt");
+        editFirstNumber = findViewById(R.id.editFirstNumber);
+        editSecondNumber = findViewById(R.id.editSecondNumber);
+        textSeeResults = findViewById(R.id.textSeeResult);
+        }
 
-    }
+
 }
